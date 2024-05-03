68281fe717a9b915a66ef54905cfaa7f95be4901
mateodonino <mateodonino@gmail.com>
2024-05-03 19:47:06 -0300

    Actualización 1.8

--- app/(home)/components/Experience.tsx
+++ app/(home)/components/Experience.tsx
@@ -79,7 +79,7 @@ export default function Experience() {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="w-4 h-4"
+                  className="w-4 h-4 hover:scale-125 transition-all"
                 >
                   <path
                     strokeLinecap="round"
@@ -100,7 +100,7 @@ export default function Experience() {
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="w-4 h-4"
+                  className="w-4 h-4 hover:scale-125 transition-all"
                 >
                   <path
                     strokeLinecap="round"
