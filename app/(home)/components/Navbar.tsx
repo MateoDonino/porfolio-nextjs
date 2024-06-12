08a0b5c4f039fbf8f9ed2b502beb44b19459f382
mateodonino <mateodonino@gmail.com>
2024-06-12 14:06:31 -0300

    v2.6

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -23,36 +23,28 @@ export default function Navbar({ className }: { className?: string }) {
   ];
 
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 2.5 }}
-    >
-      <nav
-        className={cn(" py-10 flex justify-between items-center", className)}
+    <nav className={cn(" py-10 flex justify-between items-center", className)}>
+      <a
+        href="#"
+        className="hover:scale-125 transition-all text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2"
       >
-        <a
-          href="#"
-          className="hover:scale-125 transition-all text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2"
-        >
-          Portfolio 👩🏻‍💻
-        </a>
-        <div className="flex items-center gap-5">
-          {socials.map((social, index) => {
-            const Icon = social.Icon;
-            return (
-              <Link
-                href={social.link}
-                key={index}
-                aria-label={social.label}
-                target={social.target}
-              >
-                <Icon className="w-5 h-5 hover:scale-150 transition-all" />
-              </Link>
-            );
-          })}
-        </div>
-      </nav>
-    </motion.div>
+        Portfolio 👩🏻‍💻
+      </a>
+      <div className="flex items-center gap-5">
+        {socials.map((social, index) => {
+          const Icon = social.Icon;
+          return (
+            <Link
+              href={social.link}
+              key={index}
+              aria-label={social.label}
+              target={social.target}
+            >
+              <Icon className="w-5 h-5 hover:scale-150 transition-all" />
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
   );
 }
